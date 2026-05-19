5090b7a568550a003b864d92e90fb46881766833
Jesper Wicklund <jesper.wicklund@outlook.com>
2026-05-19 12:01:48 +0200

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="flex h-screen bg-zinc-950 text-white">
+      <aside className="w-80 border-r border-zinc-800 p-4">
+        <h1 className="text-2xl font-bold">
+          Smart Stockholm Map
+        </h1>
+      </aside>
+
+      <main className="flex-1">
+        <div className="flex h-full items-center justify-center">
+          MAP COMING SOON
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
